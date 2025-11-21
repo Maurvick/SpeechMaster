524eb59147dcb9f205d42bfc607cc5e4e0205a0c
Maurvick <82706197+Maurvick@users.noreply.github.com>
2025-11-21 15:59:37 +0200

    push changes

---
+++ ExperimentASR/Services/DatabaseService.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Data.Sqlite;
+
+namespace ExperimentASR.Services
+{
+    public class DatabaseService
+    {
+        private readonly string _connectionString = "Data Source=MyNewDatabase.db";
+
+        private void ConnectDB()
+        {
+            try
+            {
+                using (var connection = new SqliteConnection(_connectionString))
+                {
+                    connection.Open();
+                    var command = connection.CreateCommand();
+                    command.CommandText = "CREATE TABLE IF NOT EXISTS Settings (Key TEXT, Value TEXT)";
+                    command.ExecuteNonQuery();
+                }
+            }
+            catch (SqliteException ex)
+            {
+                throw new Exception("Database connection failed: " + ex.Message);
+            }
+        }
+
+        private 
+    }
+}
